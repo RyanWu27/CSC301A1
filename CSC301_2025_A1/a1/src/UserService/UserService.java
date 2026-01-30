478d773cd08433bc5519e6634f17ba5e3ac972bd
RyanWu27 <ryanwu0484@gmail.com>
2026-01-30 13:06:39 -0500

    Changed SHA256 to uppercase even though name is lower case.

--- CSC301_2025_A1/a1/src/UserService/UserService.java
+++ CSC301_2025_A1/a1/src/UserService/UserService.java
@@ -347,7 +347,7 @@ public class UserService { // All programs for UserService
             MessageDigest md = MessageDigest.getInstance("SHA-256");
             byte[] hash = md.digest(s.getBytes(StandardCharsets.UTF_8));
             StringBuilder sb = new StringBuilder();
-            for (byte b : hash) sb.append(String.format("%02x", b));
+            for (byte b : hash) sb.append(String.format("%02X", b));
             return sb.toString();
         } catch (Exception e) {
             throw new RuntimeException(e);
